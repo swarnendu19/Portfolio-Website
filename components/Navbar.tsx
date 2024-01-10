29fb38dea389d94acc3c202f21e7766298266306
Swarnendu <swarnendumaity633@gmail.com>
2024-01-10 12:26:00 +0530

    Project

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,6 +2,7 @@
 
 import { Socials } from "@/constants";
 import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 const Navbar = () => {
@@ -9,13 +10,14 @@ const Navbar = () => {
     <div className="fixed top-0 z-[40] w-full h-[100px] bg-transparent flex justify-between items-center px-10 md:px-20">
       <div className="flex flex-row gap-3 items-center">
         <div className="relative">
-          {/* <Image
-            src="/horseLogo.jpg"
+          { <Image
+          
+            src="/myImage1.png"
             alt="logo"
             width={40}
             height={40}
             className="w-full h-full object-contain rounded-full"
-          /> */}
+          /> }
         </div>
         <h1 className="text-white text-[25px] font-bold">
           Swarnendu{" "}
