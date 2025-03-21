c85374b62b9b0dbc90e0b3d6c6f0a58a989cd4ff
Swarnendu <swarnendumaity633@gmail.com>
2025-03-21 21:52:49 +0530

    update experience

--- src/data/experiences.ts
+++ src/data/experiences.ts
@@ -13,7 +13,18 @@ const experiences: IExperienceItem[] = [
     description:
       "As a Full Stack Development Intern, I had the invaluable opportunity to make a significant impact by revamping and enhancing SaaS SEO tool product using React and FastAPI, showcasing my expertise in the field of Web development. I played a pivotal role in the development process by deploying RESTful APIs, enabling seamless integration between frontend and servers, thereby enhancing the overall user experience and functionality of these app.My contributions as an intern reflect my dedication to staying current with modern tech stacks and best practices, ultimately delivering meaningful results to the projects I worked on.",
   },
-  
+  {
+    designation: "Freelancing in Full Stack Development",
+    company: "Myself",
+    startDate: "April 2025",
+    endDate: "Ongoing",
+    isCurrentJob: true,
+    location: "Remote",
+    shortDescription:
+      "Working on CRM Software and Gaming and building a SAAS product with team",
+    description:
+      "Worked with over five clients, delivering high-quality software solutions. Most recently, developed a gaming web application and a CRM software from scratch, ensuring robust functionality, scalability, and an intuitive user experience."
+  },
 ];
 
 export default experiences;
